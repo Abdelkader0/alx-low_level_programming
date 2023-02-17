3c8e4aa2b822470651be527105a245a9ed342911
Abdelkader0 <ahmedbob43@gmail.com>
2023-02-17 15:23:17 -0800

    0x01-variables_if_else_while Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
- /**
-  * main - prints alphabet but q and e
-  *
-  * Return: Always 0 (success)
-  */
+/**
+ * main - prints alphabet but q and e
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
-char c = 'a';
+  char c = 'a';
 
-while (c <= 'z')
-{
-if (c == 'q' || c == 'e')
+  while (c <= 'z')
+    {
+      if (c == 'q' || c == 'e')
+	{
+	  c++;
+	  continue;
+	}
+      else
+	{
+	  putchar(c);
+	  c++;
+	}
+    }
 
-c++;
-continue;
-}
-else
-{
-putchar(c);
-c++;
-}
-}
-
-putchar('\n');
-return (0);
+  putchar('\n');
+  return (0);
 }
