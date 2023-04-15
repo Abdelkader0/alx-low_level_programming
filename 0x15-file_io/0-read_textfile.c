abe4aa4dcaf52d3fb142ce4aa4cfb53b48480100
Abdelkader0 <ahmedbob43@gmail.com>
2023-04-14 18:05:06 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,44 @@
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a textfile and prints it to the POSIX standard output.
- * @filename: The filename.
- * @letters: The number of letters to read and print.
- * Return: The actual number of letters it could read and print if everything
- *	   runs well. 0 otherwise.
- */
+  * read_textfile - reads a text file and prints it to the POSIX standard out
+  * @filename: name of the file to read
+  * @letters: number of characters to print
+  * Return: 0 on success
+  **/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int tf, res, rs;
 	char *buffer;
-	int fd, tmp = 1;
-	size_t read_size = 0;
 
-	printf("la taille est %ld", letters);
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+
+	tf = open(filename, O_RDONLY);
+	if (tf < 0)
 		return (0);
-	buffer = malloc(letters * sizeof(char));
-	buffer[letters] = '\0';
-	do {
-		tmp = read(fd, buffer, letters);
-		read_size += tmp;
-	} while (tmp);
-	close(fd);
-	if (read_size != letters)
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
 		return (0);
-	write(1, buffer, letters);
-	return (read_size);
+
+	res = read(tf, buffer, letters);
+	if (res < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	buffer[res] = '\0';
+	close(tf);
+	rs = write(STDOUT_FILENO, buffer, res);
+	if (rs < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	return (rs);
 }
